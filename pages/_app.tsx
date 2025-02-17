803e003b34fee5a2277500eef7c2d5d69063fc24
Cássia <cascrisbern@gmail.com>
2025-02-17 21:21:22 +0200

    added home to logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css'
 import logo from '../assets/logo.svg'
 import { AppProps } from 'next/app'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='flex flex-col items-start min-h-screen '>
       <div className='py-8 px-0 w-full max-w-6xl my-0 mx-auto '>
-        <Image src={logo} alt="" />
+        <a href={'/'} className='cursor-pointer'>
+          <Image src={logo} alt="" />
+        </a>
       </div>
 
       <Component {...pageProps} /> 
