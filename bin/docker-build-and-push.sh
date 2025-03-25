411c0af3aed20390fc5d76a1cf9df26a59277f38
choosy <stefan.horomnea@daon.com>
2025-03-25 11:37:16 +0200

    bin

---
+++ bin/docker-build-and-push.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+docker build . -t ignite-shop:latest
+docker tag hocli-ui:latest registry.digitalocean.com/isainho/ignite-shop:latest
+docker push registry.digitalocean.com/isainho/ignite-shop:latest
