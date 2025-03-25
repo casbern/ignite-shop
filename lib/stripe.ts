02b5bae49763cb1c66c34a6027ca906ab352a178
choosy <stefan.horomnea@daon.com>
2025-03-25 12:24:53 +0200

    fix stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = process.env.STRIPE_API_KEY
-  ? new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2025-01-27.acacia",
-      appInfo: {
-        name: "Ignite Shop",
-      },
-    })
-  : null;
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-01-27.acacia",
+  appInfo: {
+    name: "Ignite Shop",
+  },
+});
