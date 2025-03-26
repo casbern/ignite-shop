598a945a2422aa68016dade954e89862734965d6
Cássia <cascrisbern@gmail.com>
2025-03-26 17:33:15 +0200

    fix: logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import logo from '../assets/logo.svg'
 import { AppProps } from 'next/app'
 //import Image from 'next/image'
 import { CartProvider } from '../context/CartContext'
@@ -12,7 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <div className='flex flex-col items-start min-h-screen px-4 md:px-0 '>
           <div className='py-8 px-0 w-full max-w-6xl my-0 mx-auto flex justify-between'>
             <a href={'/'} className='cursor-pointer'>
-              <img src={logo} alt="" />
+              <img src="/logo.svg" width={140} height={140} alt="" />
             </a>
 
             <CartButton />
